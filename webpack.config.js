30cc566116a9bb5a32893c79ba226f3933a868b6
famaridon <famaridon@visiativ.com>
2017-01-29 16:10:57 +0100

    start getting image

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,20 @@
 module.exports = {
-    entry: "./src/main.ts",
-    output: {
-        filename: "bug-catcher.js"
-    },
-    resolve: {
-        // Add '.ts' and '.tsx' as a resolvable extension.
-        extensions: ["", ".webpack.js", ".web.js", ".ts", ".js"]
-    },
-    module: {
-        loaders: [
-            // all files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'
-            { test: /\.tsx?$/, loader: "ts-loader" }
-        ]
+  entry: "./src/ts/main.ts",
+  output: {
+    filename: "dist/js/bug-catcher.js"
+  },
+  resolve: {
+    // Add '.ts' and '.tsx' as a resolvable extension.
+    extensions: ["", ".webpack.js", ".web.js", ".ts", ".js"]
+  },
+  module: {
+    loaders: [
+      // all files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'
+      { test: /\.tsx?$/, loader: "ts-loader" },
+      { test: /\.scss$/, loaders: ["style-loader", "css-loader", "sass-loader"] }
+    ],
+    sassLoader: {
+      includePaths: ["./src/css"]
     }
+  }
 }

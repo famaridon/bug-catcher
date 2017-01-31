0eee3ac59c9a12b2d1d1d493509ce0999012c1eb
famaridon <famaridon@visiativ.com>
2017-01-31 21:29:17 +0100

    dev now start mock server

--- mock/server.js
+++ mock/server.js
@@ -2,9 +2,11 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
+  console.log('A bug is reived : ');
+  console.log(req.params);
   res.send({id: 100, url: "https://www.google.fr", public: false});
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(3080, function () {
+  console.log('Example app listening on port 3080!');
 });
